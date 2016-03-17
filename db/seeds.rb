3b91fd124d14a243c1fa47768f8bbea00d2cdc2f
Elton Santos <elton@r2a.net.br>
2016-03-17 18:08:17 -0300

    Work with access level on groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Company.create description: "R2A", cnpj: "44332211", capital: 100000
+Company.create description: "Instituto Atlântico", cnpj: "11223344", capital: 200000
+
+User.create email: "teste@teste.com", password: "12345678", role: 0, company_id: 1
+User.create email: "fulano@fulano.com", password: "12345678", role: 1, group_id: 1, father_id: 1, company_id: 1
+
 Functionality.create description: "Editar empresa"
 Functionality.create description: "Remover empresa"
 Functionality.create description: "Editar produto"
@@ -18,9 +24,3 @@ GroupFunctionality.create group_id: 3, functionality_id: 4
 Product.create description: "Macbook Air", price: 4500, quantity: 2
 Product.create description: "Asus Zenfone 2", price: 1500, quantity: 4
 Product.create description: "Iphone 6s", price: 3500, quantity: 3
-
-Company.create description: "R2A", cnpj: "44332211", capital: 100000
-Company.create description: "Instituto Atlântico", cnpj: "11223344", capital: 200000
-
-User.create email: "teste@teste.com", password: "12345678", role: 0, company_id: 1
-User.create email: "fulano@fulano.com", password: "12345678", role: 1, group_id: 1, father_id: 1, company_id: 1
